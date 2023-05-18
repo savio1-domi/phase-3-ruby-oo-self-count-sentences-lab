8947a2944325d4558ff62b94c009fe5acc5c6366
git-savio1.domi <techsavvydominic3@gmail.com>
2023-05-18 10:30:41 +0300

    Completed assignments

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,20 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    self.nil? || self.empty? ? 0 : self.split(/[.?!]+/).count 
   end
 end
+
